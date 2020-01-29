718ccf1a37135b3569d927d4971d4d47f77ab7fe
Norberto Santiago <norbertosantiago82@gmail.com>
2020-01-29 03:57:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,6 +7,5 @@ end
 def batch_badge_creator(array)
   new_array []
   array.each do |name|
-    new_array.push 
-  return "Hello, my name is #{speakers}."
+    new_array.push("Hello, my name is #{speakers}.")
 end
